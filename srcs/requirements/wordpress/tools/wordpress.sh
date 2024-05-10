f44732cde97320627e3c7d865ddb74f3e770dd41
selfix123 <lezach33@hotmail.com>
2024-05-10 16:25:55 -0400

    done

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -18,7 +18,7 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 
     wp core install --allow-root --url="${URL}" --title="${WP_TITLE}" --admin_user="${WP_ADMIN}" --admin_password="${WP_PASSWORD}" --admin_email="${WP_EMAIL}" --skip-email
 
-    wp user create "${WP_USER}" "${WP_EMAIL}" --user_pass="${WP_USER_PASSWORD}" --allow-root --role=author
+    wp user create "${WP_USER}" "${WP_USER_EMAIL}" --user_pass="${WP_USER_PASSWORD}" --allow-root --role=author
 
 fi
 
